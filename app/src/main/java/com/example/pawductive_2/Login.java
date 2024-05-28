76dc1a8eacd49946c4f9267b1536d3f3b6dca704
Simon Escano <escanosimonlyster@gmail.com>
2024-05-29 03:49:17 +0800

    added popup

--- app/src/main/java/com/example/pawductive_2/Login.java
+++ app/src/main/java/com/example/pawductive_2/Login.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.pawductive_2.R;
 
 public class Login extends AppCompatActivity {
     TextView txtSignUp;
+    LinearLayout btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,20 @@ public class Login extends AppCompatActivity {
 
         txtSignUp = findViewById(R.id.txtSignUp);
 
+        btnSignup = findViewById(R.id.btnSignUp);
+
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, Register.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Tasks.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/pawductive_2/Tasks.java
+++ app/src/main/java/com/example/pawductive_2/Tasks.java
@@ -2,18 +2,39 @@ package com.example.pawductive_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
-
-import com.example.pawductive_2.R;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Tasks extends AppCompatActivity {
+
+    LinearLayout btnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5_tasks);
+
+        btnAddTask = findViewById(R.id.btnAddTask);
+        btnAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popupView = inflater.inflate(R.layout.popup_create_task, null);
+
+                int width = ViewGroup.LayoutParams.MATCH_PARENT;
+                int height = ViewGroup.LayoutParams.WRAP_CONTENT;
+                PopupWindow createTaskPopup = new PopupWindow(popupView, width, height, true);
+
+                createTaskPopup.showAtLocation(findViewById(R.id.tasks), Gravity.CENTER_VERTICAL, 0, 0);
+            }
+        });
     }
 }
